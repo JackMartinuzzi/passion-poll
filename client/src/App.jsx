e476e22fc19c71c582441ca697707637986b8644
JackMartinuzzi <jjmartinuzzi@gmail.com>
2023-06-06 17:53:32 -0600

    Added airbnb linter and MUI for components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Bubble from './Bubble.jsx';
+import InputForm from './InputForm.jsx';
 
 function App() {
   return (
     <div>
-      Hello!
+      <InputForm />
+      <Bubble/>
     </div>
-  )
-};
+  );
+}
 
 export default App;
